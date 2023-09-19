8dfed5ca2f847a07489989deb5166bb4c8514255
Ezra Machoka <ezra.machoka@student.moringaschool.com>
2023-09-20 01:10:11 +0300

    create button

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
 
 messages = [{'title': 'Message One',
              'content': 'Message One Content'},
@@ -15,4 +17,16 @@ def index():
 
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+
     return render_template('create.html')
